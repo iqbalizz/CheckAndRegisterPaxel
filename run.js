536b253f9ec344104cdae2d530f85d3374c7ba9d
iqbalizz <iqbalizzudin13@gmail.com>
2023-09-07 16:46:24 +0700

    Update script run.js, change inquirer and add banner.js

--- run.js
+++ run.js
@@ -3,6 +3,8 @@ import chalk from "chalk";
 import readlineSync from "readline-sync";
 import { v4 as uuidv4 } from "uuid";
 import inquirer from "inquirer";
+// import { getOtpRequest, getValidateOtp, getRegisterAcc, getLogin } from "./utils/httpRequest.js";
+import { banner1, banner2, banner } from "./utils/banner.js";
 
 const getOtpRequest = (inputNomer, xPlayer) => new Promise((resolve, reject) => {
     const dataString = `{"phone":"${inputNomer}"}`
@@ -67,25 +69,51 @@ const getRegisterAcc = (maskingNumber, inputPIN, inputNomer, sendBy, inputOtp, x
         .catch(error => reject(error))
 });
 
-(async () => {
-    const answer = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'selectedOption',
-            message: 'Pilih salah satu opsi:',
-            choices: [
-                'Register Account Paxel',
-                'Login Account Paxel',
-            ]
-        }
-    ]);
+//!Login Account
+const getLogin = (inputPinLogin, inputNomerLogin, xPlayer) => new Promise((resolve, reject) => {
+    const dataString = `{"pin":"${inputPinLogin}","phone":"${inputNomerLogin}"}`
+    fetch(`https://api.paxel.co/apg/api/v3/login`, {
+        method: 'POST',
+        headers: {
+            'Host': 'api.paxel.co',
+            'X-Version': '2.18.2',
+            'Device-Id': '02:00:00:00:00:00',
+            'Device-Type': 'Phone',
+            'X-Player': xPlayer,
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Content-Length': '40',
+            'Accept-Encoding': 'gzip, deflate',
+            'User-Agent': 'okhttp/4.7.2'
+        },
+        body: dataString
+    })
+        .then(res => resolve(res.json()))
+        .catch(error => reject(error))
+});
 
-    if (answer.selectedOption === 'Register Account Paxel') {
+(async () => {
+    console.log(`${chalk.green(banner())}`)
+    const pilihan1 = `[1] ${chalk.green(`Register Account Paxel`)}`;
+    const pilihan2 = `[2] ${chalk.green(`Login Account Paxel`)}`
+    console.log(`${pilihan1}\n${pilihan2}`)
+    let inputPilihan;
+    let isValid = false
+    while (!isValid) {
+        inputPilihan = readlineSync.questionInt(`[?] Masukkan pilihan (1/2) : `)
+        if (inputPilihan !== 1 && inputPilihan !== 2) {
+            console.log(`[!] ${chalk.red(`Harus memilih 1 atau 2`)}`);
+        } else {
+            isValid = true;
+        }
+    }
+    if (inputPilihan === 1) {
+        console.log(banner1())
         //!generate Xplayer
         const xPlayer = uuidv4();
         //!Input User
         const inputNomer = readlineSync.question(`[?] Masukkan Nomer Kamu : `)
         const resultOtpRequest = await getOtpRequest(inputNomer, xPlayer);
+        // console.log(resultOtpRequest)
         const getData = resultOtpRequest.data;
         const message = resultOtpRequest.code_message;
         const maskingNumber = getData.masking_number;
@@ -107,13 +135,24 @@ const getRegisterAcc = (maskingNumber, inputPIN, inputNomer, sendBy, inputOtp, x
                     console.log(`[!] ${chalk.red(`Register Gagal!`)}`)
                 }
             } else {
-                console.log(`[!] ${chalk.red(`Gagal Menginputkan PIN!`)}`)
+                console.log(`[!] ${chalk.red(`Gagal Menginputkan PIN! Masukkan code OTP yang benar!`)}`)
             }
         } else {
             console.log(`[!] ${chalk.red(`Gagal mengirim OTP!`)}`)
         }
-    } else if (answer.selectedOption === 'Login Account Paxel') {
+    } else if (inputPilihan === 2) {
+        console.log(banner2());
+        //!generate Xplayer
+        const xPlayer = uuidv4();
+        const inputNomerLogin = readlineSync.question(`[?] Masukkan Nomer Kamu Untuk Login : `);
+        const inputPinLogin = readlineSync.question(`[?] Masukkan PIN Account : `)
+        const resultLogin = await getLogin(inputPinLogin, inputNomerLogin, xPlayer)
+        const statusLogin = resultLogin.code_message;
+        if (resultLogin.code === 200) {
+            console.log(`[!] ${chalk.green(statusLogin)}`)
+            // console.log(resultLogin)
 
+        }
     }
 
 })();
